f96aba47be5cda95326f76e17b1c76dd46b43324
Mukul Chugh <mukulchughofficial@gmail.com>
2021-12-04 08:06:04 +0530

    Header Tab Component

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
+import HeaderTabs from "../components/HeaderTabs";
 
 export default function Home() {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+      <View style={{ backgroundColor: "white", padding: 15 }}>
+        <HeaderTabs />
+      </View>
+    </SafeAreaView>
   );
 }
