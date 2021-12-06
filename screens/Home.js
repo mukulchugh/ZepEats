9bf26c035a1378078242aa93d6b51c8a9fefb635
Mukul Chugh <mukulchughofficial@gmail.com>
2021-12-07 02:01:44 +0530

    Search Bar added with vector Icon support

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
 import HeaderTabs from "../components/HeaderTabs";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "white", padding: 15 }}>
         <HeaderTabs />
+        <SearchBar />
       </View>
     </SafeAreaView>
   );
